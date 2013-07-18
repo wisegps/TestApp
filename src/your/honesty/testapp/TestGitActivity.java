433e1c6b1099b9d02e1dfbf94276bd4db5a3d438
wisegps <wisegps@333>
2013-07-18 14:31:17 +0800

    test1

--- src/your/honesty/testapp/TestGitActivity.java
+++ src/your/honesty/testapp/TestGitActivity.java
@@ -2,6 +2,7 @@ package your.honesty.testapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class TestGitActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +10,6 @@ public class TestGitActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Button button = (Button)findViewById(R.id.bt_me);
     }
 }
